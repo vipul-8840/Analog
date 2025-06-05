fb244cbb73131d5851b77a2ae2c6c3836ef32264
vipul-8840 <vipulkumarlen@gmail.com>
2025-06-06 00:31:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App()
+{
+    return (
+        <div className='bg-black min-h-screen text-white' >
+          <h1 >Hello World</h1>
+        </div>
+    )
+}
+      
+export default App
