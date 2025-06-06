1ee4330552a96ee1937feb31fd9d24f80d4a26ae
vipul-8840 <vipulkumarlen@gmail.com>
2025-06-07 01:25:39 +0530

    making circle

---
+++ src/clock.jsx
@@ -0,0 +1,22 @@
+
+function Clock()
+{
+
+
+   return (
+                <div className="bg-gray-900 min-h-screen flex flex-col justify-center items-center  text-white text-3xl">
+                    <div className=" w-64 h-64 rounded-full border-4 border-white flex items-center justify-center ">
+                        <div className="absolute w-3 h-3 bg-white rounded-full z-50" />~
+
+                    </div>
+                    
+                    
+                  
+                     
+                </div>
+   )
+
+
+}
+
+export default Clock;
